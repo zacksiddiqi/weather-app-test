9244d70781fa9c2241fd4790d6d88b4525a135b8
zackary.siddiqi.22 <zackary.siddiqi.22@cnu.edu>
2026-05-07 19:08:37 -0400

    implemented a GUI using tkinter

--- weather_app.py
+++ weather_app.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import requests
 from dotenv import load_dotenv
 import os
@@ -5,16 +6,42 @@ import os
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-city = input("Enter a City: ")
+def get_weather():
+    city = entry.get()
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
-response = requests.get(url)
+    response = requests.get(url)
 
-data = response.json()
+    data = response.json()
 
-if response.status_code == 200:
-    temp = round(data["main"]["temp"])
-    print(f"Temperature in {city}: {temp}°C")
-else:
-    print(f"Error: {data['message']}")
+    if response.status_code == 200:
+        temp = round(data["main"]["temp"])
+        result_label.config(text=f"The current tempurature in {city} is {temp}°C")
+    else:
+        result_label.config(text="city not found")
+
+
+window = tk.Tk()
+window.title("Zack's Weather App")
+window.geometry("350x250")
+ 
+
+label = tk.Label(window, text="Enter a City: ", font=("Arial", 14))
+label.pack(pady=10)
+
+entry = tk.Entry(window, font=("Arial", 14))
+entry.pack(pady=10)
+
+button = tk.Button(window, text="Get Temp", command=get_weather)
+button.pack()
+
+city_label = tk.Label(window, text="", font=("Arial", 14))
+city_label.pack(pady=5)
+
+result_label = tk.Label(window, text="", font=("Arial", 12, "bold"))
+result_label.pack()
+
+
+
+window.mainloop()
